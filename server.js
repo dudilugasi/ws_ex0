125995a48221f26fb99521e49ca485156c5f542f
Dudi Lugasi <dulu123@gmail.com>
2015-04-23 21:31:14 +0300

    check

--- server.js
+++ server.js
@@ -12,21 +12,12 @@ federer.subtractMedal();//output : medals = 1
 federer.subtractMedal();//output : medals = 0
 federer.subtractMedal();// output : medals = -1 ; medals is under 0
 
-//write to log from log.txt
-var log = "";
-var stream = fs.createReadStream('log.txt').on('data',function(chunk) {
-    log += chunk;
-});
-stream.read();
-
-
 //creates a server that listens on port 3000
 http.createServer(function(res,req) {
-
-    //callback functions on new request
-    req.writeHeader(200);
-    req.end('success\n' + log);
-
+    fs.readFile('log.txt', function(err,data) {
+        req.writeHeader(200);
+        req.end('success\n' + data);
+    });
 }).listen(3000);
 
 console.log('server listens on port 3000');
